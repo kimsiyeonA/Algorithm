aa4b4cb483147f7475491d37be2b9a32bcf75e2c
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2025-06-10 15:33:49 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList solution(int start_num, int end_num) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i = start_num; i <= end_num; i++){
+            answer.add(i);
+        }
+        
+        return answer;
+    }
+}
