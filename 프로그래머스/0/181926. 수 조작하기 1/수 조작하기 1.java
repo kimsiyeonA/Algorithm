03b4e0fa491dcae0d1cd56f5cbf96b698804b3cd
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2025-09-30 20:46:28 +0900

    [level 0] Title: 수 조작하기 1, Time: 11.51 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        char w = 'w';
+        char s = 's';
+        char d = 'd';
+        char a = 'a';
+        for(int i=0;i<control.length();i++){
+            if(control.charAt(i)==w){
+                answer = answer+1;
+            }else if(control.charAt(i)==s){
+                answer = answer-1;
+            }else if(control.charAt(i)==d){
+                answer = answer+10;
+            }else if(control.charAt(i)==a){
+                answer = answer-10;
+            }
+           
+        }
+        
+        return answer;
+    }
+}
